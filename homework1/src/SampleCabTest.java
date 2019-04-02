b7a2a24ee37b76b40d4eb53168664ff5b19bb2c6
marcanio1 <35542918+marcanio1@users.noreply.github.com>
2019-04-01 23:38:54 -0500

    Add files via upload

---
+++ homework1/src/SampleCabTest.java
@@ -0,0 +1,16 @@
+import hw1.Cab;
+import java.util.Scanner;
+public class SampleCabTest
+{
+  public static void main(String[] args)
+  {
+	  double height;
+	  String name;
+	  int age, Zipcode;
+	  
+	  Scanner in = new Scanner(System.in);
+	  System.out.print("Please enter your name: ");
+	  name = in.nextLine();
+	  System.out.println(name);
+  }
+}
